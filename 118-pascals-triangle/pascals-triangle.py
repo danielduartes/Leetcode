7ec8718707963a3fdfee327b69cacb845b470624
danielduartes <duartedaniel479@gmail.com>
2025-08-12 14:12:31 -0300

    Time: 0 ms (100.00%) | Memory: 12.6 MB (20.60%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows == 2:
+            triangulo = [[1],[1,1]]
+        elif numRows == 1:
+            triangulo = [[1]]
+        elif numRows > 2:
+            triangulo = [[1],[1,1]]
+            for i in range(numRows):
+                linha = [1]
+                if i > 1:
+                    for j in range(len(triangulo[-1])-1):
+                        novo_numero = triangulo[-1][j] + triangulo[-1][j+1]
+                        linha.append(novo_numero)
+                    linha.append(1)
+                    triangulo.append(linha)
+                
+        return triangulo
